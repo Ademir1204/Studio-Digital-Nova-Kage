54a810fc22fa3810128bf6dff7e4b1ee134ad3aa
Ademir1204 <ademirjonatha2@gmail.com>
2025-12-02 20:51:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,11 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/', // ESTA É A LINHA QUE ESTAVA FALTANDO
+  plugins: [react()],
+})
